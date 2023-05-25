fea6b60f4bd6c39e9a8e3ab0d684882656be113c
BernicheAurelie <berniche.aurelie@orange.fr>
2023-05-25 09:56:21 +0200

    update requirements.txt and add performances tests

---
+++ tests/performance/locustfile.py
@@ -0,0 +1,40 @@
+from locust import HttpUser, task
+
+
+class ProjectPerfTest(HttpUser):
+    @task
+    def index(self):
+        response = self.client.get("/")
+
+    @task
+    def points(self):
+        response = self.client.get("/points")
+
+    @task
+    def showSummary(self):
+        email = 'john@simplylift.co'
+        response = self.client.post('/showSummary', data={'email' : email})
+
+    @task
+    def showSummaryBadEmail(self):
+        email = 'test@test.com'
+        response = self.client.post('/showSummary', data={'email' : email})
+
+    @task
+    def logout(self):
+        response = self.client.get("/logout", follow_redirects=True)
+
+    @task
+    def purchasePlaces(self):
+        email = 'john@simplylift.co'
+        response = self.client.post('/showSummary', data={'email' : email})
+        credentials = {
+            'competition': 'Spring Festival',
+            'club': 'Simply Lift',
+            'places': '2'
+            }
+        response_2 = self.client.post('/purchasePlaces', data = credentials)
+
+    @task
+    def book(self):
+        response = self.client.get('/book/Spring%20Festival/Simply%20Lift')
